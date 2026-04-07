e4ac843be09d7eb22c6ac742fee10ad6911bb66f
mammy <duschs0701@naver.com>
2026-04-07 23:32:19 +0900

    FlowServlet Update

---
+++ Practice2/src/main/java/core/FlowServlet.java
@@ -0,0 +1,39 @@
+package core;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class FlowServlet
+ */
+@WebServlet("/FlowerServlet")
+public class FlowServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init() 메서드 호출!");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("destroy() 메서드 호출!");
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("service() 메서드 호출!");
+	}
+
+}
