27ed54a0c62ab3d9904b2dd204b55e721c0accfe
mammy <duschs0701@naver.com>
2026-04-10 22:26:45 +0900

    MemberLocalServlet Update

---
+++ Practice2/src/main/java/core/MemberLocalServlet.java
@@ -0,0 +1,32 @@
+package core;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class MemberLocalServlet
+ */
+public class MemberLocalServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	int member_v = 0;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		int local_v = 0;
+		member_v++;
+		local_v++;
+		out.print("<h2>member_v(지역변수) : " + member_v + "</h2>");
+		out.print("<h2>local_v(지역변수) : " + local_v + "</h2>");
+		out.close();
+	}
+}
