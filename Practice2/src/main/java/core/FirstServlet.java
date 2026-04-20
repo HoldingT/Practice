b7ddf8117ac453be3dca74e7f3f45dfa4a1aef56
mammy <duschs0701@naver.com>
2026-04-20 23:35:03 +0900

    Servlet Update

--- Practice2/src/main/java/core/FirstServlet.java
+++ Practice2/src/main/java/core/FirstServlet.java
@@ -26,7 +26,6 @@ public class FirstServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.setContentType("text/html; charset=utf-8");
 		PrintWriter out = response.getWriter();
 		out.println("<h3>안녕하세요?</h3>");
