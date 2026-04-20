dc4f95c25bc728fbb519ff44f806af08bf6d8e55
mammy <duschs0701@naver.com>
2026-04-20 23:50:02 +0900

    Token Update

--- Practice2/src/main/java/core/FirstServlet.java
+++ Practice2/src/main/java/core/FirstServlet.java
@@ -9,9 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class FirstServlet
- */
+
 public class FirstServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
