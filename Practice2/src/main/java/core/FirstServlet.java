31c8522ac04e140df9e8fcfcfcd51142d919aa73
mammy <duschs0701@naver.com>
2026-04-22 23:50:05 +0900

    Update

--- Practice2/src/main/java/core/FirstServlet.java
+++ Practice2/src/main/java/core/FirstServlet.java
@@ -13,16 +13,10 @@ import jakarta.servlet.http.HttpServletResponse;
 public class FirstServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
     public FirstServlet() {
         
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=utf-8");
 		PrintWriter out = response.getWriter();
