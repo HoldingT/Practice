22172f61d444c55f502781bf36789d2a01ac6698
mammy <duschs0701@naver.com>
2026-04-23 21:01:33 +0900

    CountServlet Update

---
+++ Practice2/src/main/java/core/CountServlet.java
@@ -0,0 +1,32 @@
+package core;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class CountServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession();
+		if(session.getAttribute("cnt") == null) {
+			session.setAttribute("cnt", new int[1]);
+		}
+		int[] count = (int[])session.getAttribute("cnt");
+		count[0]++;
+		out.print("<h3>당신은 "+ count[0] + "번째 방문입니다.</h3>");
+		out.close();
+	}
+
+}
