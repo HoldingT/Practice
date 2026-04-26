3cb8cfbd6083a42d4ee3cbd6bf94a989b4080479
mammy <duschs0701@naver.com>
2026-04-26 22:43:23 +0900

    CountServlet Update

--- Practice2/src/main/java/core/CountServlet.java
+++ Practice2/src/main/java/core/CountServlet.java
@@ -13,9 +13,6 @@ import java.io.PrintWriter;
 public class CountServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=utf-8");
 		PrintWriter out = response.getWriter();
